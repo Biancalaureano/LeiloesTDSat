8b828b70bfbb83e6e4b545ba75b2cd3902224886
suporte <suporte@camarapiracicaba.sp.gov.br>
2024-07-12 15:39:02 -0300

    Funcionalidade de salvar produto no banco de dados ao botão de cadastro

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/leiloes_tds_sat?user=root&password=1234");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/leiloes_tds_sat?useSSL=false&verifyServerCertificate=true" , "root" , "1234" );
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
